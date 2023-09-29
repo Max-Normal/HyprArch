3e8ada8ed0e007f1d38668da3c35f5370dfd3bbf
MaxNormal <72371299+Max-Normal@users.noreply.github.com>
2023-09-29 10:35:41 +0200

    Update 12-gitflats.sh

--- 12-gitflats.sh
+++ 12-gitflats.sh
@@ -50,8 +50,6 @@ flatpak install flathub com.github.tchx84.Flatseal
 
 flatpak install flathub com.discordapp.Discord
 
-flatpak install flathub com.parsecgaming.parsec
-
 git clone https://github.com/AdnanHodzic/auto-cpufreq.git
 cd auto-cpufreq && sudo ./auto-cpufreq-installer
 
